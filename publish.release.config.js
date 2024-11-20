a7a8e94b43303e58f8ac9ea307b314fd49351232
andyp-22 <166191160+andyp-22@users.noreply.github.com>
2024-11-21 10:34:22 +1100

    ci: Update publish.release.config.js to correctly order semantic-release plugins. (#19)

--- publish.release.config.js
+++ publish.release.config.js
@@ -3,8 +3,22 @@ const common  = require('./common.release.config.js');
 const config = {
   ...common,
   plugins: [
+    // Add the common plugins first, so the changelog plugin is called before the npm and git plugins.
     ...common.plugins,
     [
+      // The npm plugin must be called after the changelog plugin, and before the git plugin.
+      "@semantic-release/npm", {
+        npmPublish: false
+      }
+    ],
+    [
+      // The git plugin must be called after the changelog and npm plugins (in that order).
+      "@semantic-release/git", {
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    [
+      // Publish the extension to the Visual Studio Code marketplace.
       "semantic-release-vsce", {
         packageVsix: false,
         publish: true,
@@ -12,19 +26,10 @@ const config = {
       }
     ],
     [
-      "@semantic-release/git", {
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }
-    ],
-    [
+      // Publish the new release in GitHub, and attach the vsix file as a release asset.
       "@semantic-release/github", {
         assets: "*.vsix",
         addReleases: "bottom"
-      }
-    ],
-    [
-      "@semantic-release/npm", {
-        npmPublish: false
       }
     ]
   ]
